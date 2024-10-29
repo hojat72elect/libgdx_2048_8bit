99d0cd54cf99bc6d6b58d9557a43b3aae46ebe9d
hojat72elect <hojat72elect@gmail.com>
2024-10-29 12:39:33 -0400

    Cleaned and reformatted

--- core/src/com/nopalsoft/dosmil/screens/HelpScreen.java
+++ core/src/com/nopalsoft/dosmil/screens/HelpScreen.java
@@ -12,52 +12,45 @@ import com.nopalsoft.dosmil.MainGame;
 
 public class HelpScreen extends Screens {
 
-    Label lbTextHelp1;
-    Label lbTextHelp2;
-    Image imgPuzzle;
-
-    Button btBack;
+    Label labelTextHelp1;
+    Label labelTextHelp2;
+    Image imagePuzzle;
+    Button buttonBack;
 
     public HelpScreen(final MainGame game) {
         super(game);
-        lbTextHelp1 = new Label(Assets.languages.get("helpTop"),
-                Assets.labelStyleSmall);
-        lbTextHelp1.setWrap(true);
-        lbTextHelp1.setWidth(SCREEN_WIDTH - 20);
-        lbTextHelp1.setAlignment(Align.center);
-        lbTextHelp1.setPosition(
-                SCREEN_WIDTH / 2f - lbTextHelp1.getWidth() / 2f, 660);
-        lbTextHelp1.setScale(1.2f);
+        labelTextHelp1 = new Label(Assets.languages.get("helpTop"), Assets.labelStyleSmall);
+        labelTextHelp1.setWrap(true);
+        labelTextHelp1.setWidth(SCREEN_WIDTH - 20);
+        labelTextHelp1.setAlignment(Align.center);
+        labelTextHelp1.setPosition(SCREEN_WIDTH / 2f - labelTextHelp1.getWidth() / 2f, 660);
+        labelTextHelp1.setScale(1.2f);
 
-        imgPuzzle = new Image(Assets.puzzleSolved);
-        imgPuzzle.setSize(350, 350);
-        imgPuzzle
-                .setPosition(SCREEN_WIDTH / 2 - imgPuzzle.getWidth() / 2f, 290);
+        imagePuzzle = new Image(Assets.puzzleSolved);
+        imagePuzzle.setSize(350, 350);
+        imagePuzzle.setPosition(SCREEN_WIDTH / 2 - imagePuzzle.getWidth() / 2f, 290);
 
-        lbTextHelp2 = new Label(
-                Assets.languages.get("helpBottom"),
-                Assets.labelStyleSmall);
-        lbTextHelp2.setWrap(true);
-        lbTextHelp2.setWidth(SCREEN_WIDTH - 20);
-        lbTextHelp2.setAlignment(Align.center);
-        lbTextHelp2.setPosition(
-                SCREEN_WIDTH / 2f - lbTextHelp2.getWidth() / 2f, 200);
-        lbTextHelp2.setScale(1.2f);
+        labelTextHelp2 = new Label(Assets.languages.get("helpBottom"), Assets.labelStyleSmall);
+        labelTextHelp2.setWrap(true);
+        labelTextHelp2.setWidth(SCREEN_WIDTH - 20);
+        labelTextHelp2.setAlignment(Align.center);
+        labelTextHelp2.setPosition(SCREEN_WIDTH / 2f - labelTextHelp2.getWidth() / 2f, 200);
+        labelTextHelp2.setScale(1.2f);
 
-        btBack = new Button(Assets.buttonBack);
-        btBack.setSize(60, 60);
-        btBack.setPosition(SCREEN_WIDTH / 2 - 30, 80);
-        addPressEffect(btBack);
-        btBack.addListener(new ClickListener() {
+        buttonBack = new Button(Assets.buttonBack);
+        buttonBack.setSize(60, 60);
+        buttonBack.setPosition(SCREEN_WIDTH / 2 - 30, 80);
+        addPressEffect(buttonBack);
+        buttonBack.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 changeScreenWithFadeOut(MainMenuScreen.class, game);
             }
         });
-        stage.addActor(lbTextHelp1);
-        stage.addActor(lbTextHelp2);
-        stage.addActor(btBack);
-        stage.addActor(imgPuzzle);
+        stage.addActor(labelTextHelp1);
+        stage.addActor(labelTextHelp2);
+        stage.addActor(buttonBack);
+        stage.addActor(imagePuzzle);
     }
 
     @Override
@@ -68,14 +61,11 @@ public class HelpScreen extends Screens {
     }
 
     @Override
-    public void update(float delta) {
-
-    }
+    public void update(float delta) {}
 
     @Override
     public boolean keyDown(int keycode) {
         if (keycode == Keys.ESCAPE || keycode == Keys.BACK) {
-
             changeScreenWithFadeOut(MainMenuScreen.class, game);
         }
         return super.keyDown(keycode);
